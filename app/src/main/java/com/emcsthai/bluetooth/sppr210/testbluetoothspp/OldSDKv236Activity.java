37d0f5047489c2d4ff4750b0c170cebbfea3ef3d
nakharin-mac-mini <nakharin.sang@gmail.com>
2018-04-03 11:53:38 +0700

    - Smart Card completed.

--- app/src/main/java/com/emcsthai/bluetooth/sppr210/testbluetoothspp/OldSDKv236Activity.java
+++ app/src/main/java/com/emcsthai/bluetooth/sppr210/testbluetoothspp/OldSDKv236Activity.java
@@ -478,8 +478,6 @@ public class OldSDKv236Activity extends AppCompatActivity {
 
             if (v == btnRead) {
 
-//                bixolonPrinter.getSmartCardStatus();
-
                 arrByte.clear();
                 bixolonPrinter.powerUpSmartCard();
 
